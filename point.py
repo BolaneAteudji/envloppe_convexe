ae8f5765df47cd1b9f69248f30af19bdbfe69eeb
BolaneAteudji <bolanedjifa@gmail.com>
2024-10-18 14:14:22 +0200

    mise à jour du fichier main.py

---
+++ point.py
@@ -0,0 +1,21 @@
+class point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        def __str__(self):
+            return f"{self.x},{self.y}"
+        def __getitem__(self,i):
+            if i==0:
+                return self.x
+            if i==1:
+                return self.y
+             def 
+            
+p=point(1,2) 
+print(p)
+        
+         #ajout de la methode pour vevifier si deux sont égaux
+         
+         
+        
+        
