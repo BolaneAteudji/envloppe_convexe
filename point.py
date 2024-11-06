2f4a302332cc3d5b693b5241f1053509ac7ab6df
BolaneAteudji <bolanedjifa@gmail.com>
2024-11-06 14:49:07 +0100

    mise à jour du fichier point.py

--- point.py
+++ point.py
@@ -1,17 +1,32 @@
-class point:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
+import cmath        
+import math
+class Point:
+    def __init__(self,x,y=None):
+        if isinstance(seelf, x, Point):
+
+          self.x=x.x
+          self.y=y.y
+        else:
+            self.x=x
+            self.y=y
+              
         def __str__(self):
-            return f"{self.x},{self.y}"
+            return f"({self.x},{self.y})"
         def __getitem__(self,i):
             if i==0:
                 return self.x
             if i==1:
                 return self.y
-             def 
+            else:
+                raise IndexError("Index out of range. Use 0 for x and 1 for y.")
+        def __str__(self):
+            return f"({self.x}, {self.y})"
+        def __eq__(self,orther):
+            return isinstance(orther, Point) and self.x==orther.x and self.y==orther.y
+        def angle(self,p):
             
-p=point(1,2) 
+p1=Point(1,2) 
+p2=Point(3,4)
 print(p)
         
          #ajout de la methode pour vevifier si deux sont égaux
